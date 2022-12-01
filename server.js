2e8d74e1fbfbf8a5ff252ed3e099ef3dc9b4e251
jonathan-crepeau <jon.crepeau@gmail.com>
2022-11-30 16:39:36 -0800

    logger middleware function

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const app = express();
 const PORT = process.env.PORT || 4002;
 
 const routes = require('./routes');
+const logger = require('./middleware/utils');
 
 // Serve Static
 app.use(express.static(`${__dirname}/public`));
+
+// SECTION Middleware
+app.use(logger);
+app.use(express.json());
 
 // SECTION View Routes
 app.use('/', routes.views);
