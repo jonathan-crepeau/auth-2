646c2b8121dd046ec6ec48ba9526b5d33aeb521a
jonathan-crepeau <jon.crepeau@gmail.com>
2022-12-07 16:06:19 -0800

    include express session / mongo-connect + complete js logic for login

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,1 +1,45 @@
 console.log('login.js reporting for duty..');
+
+const form = document.getElementById('login-form');
+
+const handleSubmitClick = (event) => {
+    let formIsValid = true;
+    const userData = {};
+    event.preventDefault();
+    console.log('Sign in button was clicked');
+
+    const formInputs = [...form.elements];
+    formInputs.forEach((input) => {
+        if (input.type !== 'submit' && input.value === '') {
+            formIsValid = false;
+        } else if (input.type === 'password' && input.value.length < 4) {
+            formIsValid = false;
+        }
+
+        if (formIsValid) {
+            userData[input.name] = input.value;
+        }
+    });
+
+    if (formIsValid) {
+        console.log('Submitting user data..');
+        fetch('http://localhost:4002/api/v1/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'credentials': 'include',
+            },
+            body: JSON.stringify(userData)
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.status === 200) {
+                    window.location = '/profile'
+                }
+                console.log(data);
+            })
+            .catch((error) => console.log(error));
+    }
+}
+
+form.addEventListener('submit', handleSubmitClick);
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const PORT = process.env.PORT || 4002;
 
 const routes = require('./routes');
@@ -11,6 +13,20 @@ app.use(express.static(`${__dirname}/public`));
 // SECTION Middleware
 app.use(logger);
 app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+// SECTION User Session
+app.use(session({
+    store: MongoStore.create({
+        mongoUrl: "mongodb+srv://jonathan-crepeau:my-password@authocluster0.zfoiawz.mongodb.net/auth-app2?retryWrites=true&w=majority"
+    }),
+    secret: 'catattack',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 2
+    }
+}));
 
 // SECTION View Routes
 app.use('/', routes.views);
